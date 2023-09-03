33ca8125f58479fe73d3a747b58536f9db5f1d71
bayushkibret <bayush166@gmail.com>
2023-09-03 15:45:16 +0300

    Update settings.py

--- wedaj/settings.py
+++ wedaj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('django-insecure-k9g5eb_ikw0l7ohdyzvtr(qv8kdb(br7m+q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('0.0.0.0')
 if RENDER_EXTERNAL_HOSTNAME:
