c12ee78438680da359d772afca5dee29e47b4c43
bayushkibret <bayush166@gmail.com>
2023-09-03 16:27:58 +0300

    Update settings.py

--- wedaj/settings.py
+++ wedaj/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = 'assets'
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
